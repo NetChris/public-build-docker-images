39511a31caade8be38152370dfe21b5610099e64
Chris Simmons <chris.dev@netchris.com>
2019-12-30 11:14:37 -0800

    New BUILD_IMAGE scheme

--- docker/build_scripts/variables.sh
+++ docker/build_scripts/variables.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export BUILD_IMAGE=${BASE_BUILD_IMAGE}/${VARIANT}:${REF_TAG}-${BUILD_ID}
+# This is a good standard method for constructing the BUILD_IMAGE
+export BUILD_IMAGE=${BASE_BUILD_IMAGE}/${REF_TAG}/${VARIANT}:${BUILD_ID}
 export IMAGE_AUTHORS="NetChris Corp. (dockerimages@netchris.com)"
 export IMAGE_BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 export IMAGE_BUILD_ID=$BUILD_ID
