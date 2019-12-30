0fa527a72ce03520aad4a9780c31c8025a7f61a8
Chris Simmons <chris.dev@netchris.com>
2019-12-30 11:09:34 -0800

    Comment
    [skip ci]

--- common/build.sh
+++ common/build.sh
@@ -8,6 +8,7 @@
 
 . "./common/variables.sh"
 
+# We use the scripts from the Docker variant as they will be used similarly from the end-use builds
 . "./docker/build_scripts/variables.sh"
 
 . "./docker/build_scripts/functions.sh"
