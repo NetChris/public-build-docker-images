1fda3a661ce1fc6d0622675c8745bfb5506f63ff
Chris Simmons <chris.dev@netchris.com>
2019-12-31 06:32:15 -0800

    WIP

---
+++ docker/image_root/opt/netchris/build/scripts/sets.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# See https://stackoverflow.com/a/2871034/208990
+
+# exit immediately when a command fails
+set -e
+
+# treat unset variables as an error and exit immediately
+set -u
+
+# print each command before executing it
+set -x
