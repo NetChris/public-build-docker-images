06e318fca74db532b7c0c27526a7ecf9cb32601a
Chris Simmons <gitlab.com@netchris.com>
2019-12-31 21:13:19 +0000

    Add IMAGE_BUILD_ID_PREFIX

--- docker/variables.sh
+++ docker/variables.sh
@@ -3,5 +3,7 @@
 
 export VARIANT="docker"
 export IMAGE_TITLE="Docker Image for build/deploy - Docker variant"
-export FROM_IMAGE="docker:stable"
+DOCKER_VERSION="stable"
+export IMAGE_BUILD_ID_PREFIX=${DOCKER_VERSION}
+export FROM_IMAGE="docker:${DOCKER_VERSION}"
 export IMAGE_VENDOR="Docker"
