609cce01a8672018ca901df3d07a3ece21c2dfbb
Chris Simmons <chris.dev@netchris.com>
2019-12-31 09:02:21 -0800

    TODOs
    [skip ci]

--- common/local/variables.sh
+++ common/local/variables.sh
@@ -4,7 +4,6 @@ EPOCH=$(date +%s)
 export REF_TAG=local
 export BUILD_ID=$EPOCH
 
-# TODO - All below belong in docker/
 export BASE_BUILD_IMAGE="registry.gitlab.com/cssl/netchris/public/build/docker/images"
 export IMAGE_REVISION=$BUILD_ID
 export CI_REGISTRY=registry.gitlab.com
