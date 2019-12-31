c7795d8c93372f7e927a03db16d288a3daee9a06
Chris Simmons <chris.dev@netchris.com>
2019-12-30 17:17:37 -0800

    Scripting

--- docker/test-build.sh
+++ docker/test-build.sh
@@ -7,7 +7,7 @@
 # TODO - Watch out for circ reference
 . "./common/variables.sh"
 
-. "./opt/scripts/netchris/variables/docker-build.sh"
-. "./opt/scripts/netchris/functions/docker.sh"
+. "/opt/scripts/netchris/variables/docker-build.sh"
+. "/opt/scripts/netchris/functions/docker.sh"
 
 docker_image_build
