a8258008977a3b4b6ee74b651b963f8846f29039
Chris Simmons <chris.dev@netchris.com>
2019-12-31 07:35:42 -0800

    Test tweak

--- docker/test-build.sh
+++ docker/test-build.sh
@@ -9,4 +9,7 @@
 
 . "/opt/netchris/build/scripts/docker/functions.sh"
 
+# Munge BUILD_IMAGE so that the image we build isn't used later
+export BUILD_IMAGE=${BUILD_IMAGE}-TESTBUILD
+
 docker_image_build
