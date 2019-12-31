e832177f2091693553f70c5b24e75bf1efa07a0f
Chris Simmons <chris.dev@netchris.com>
2019-12-31 08:17:10 -0800

    Build an actual test Docker image

---
+++ test-docker/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Tests that can safely be run to ensure proper script configuration.
+
+. "/opt/netchris/build/scripts/common/sets.sh"
+
+export VARIANT="test-docker"
+
+. "./common/variables.sh"
+
+. "/opt/netchris/build/scripts/docker/variables/build.sh"
+
+. "/opt/netchris/build/scripts/docker/functions.sh"
+
+# Munge BUILD_IMAGE so that the image we build isn't used later
+export BUILD_IMAGE=${BUILD_IMAGE}-TESTBUILD
+
+docker_image_build
