9548ef1f589ab3eb30622a22669728e5d714e41b
Chris Simmons <chris.dev@netchris.com>
2019-12-31 09:19:09 -0800

    GitLab variables

---
+++ docker/image_root/opt/netchris/build/scripts/docker/variables/gitlab.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Establish required variables from GitLab well-known environment variables
+
+export BASE_BUILD_IMAGE="${CI_REGISTRY_IMAGE}"
+export IMAGE_SOURCE_REPOSITORY="${CI_PROJECT_URL}"
+export IMAGE_REVISION="${CI_COMMIT_SHA}"
+export DOCKER_REGISTRY="${CI_REGISTRY}"
+export DOCKER_REGISTRY_PASSWORD="${CI_REGISTRY_PASSWORD}"
+export DOCKER_REGISTRY_USERNAME="${CI_REGISTRY_USER}"
