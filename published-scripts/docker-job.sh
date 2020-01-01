9265da7d4d1bf9f9302e32f086675000992570f8
Chris Simmons <chris.dev@netchris.com>
2020-01-01 10:12:16 -0800

    Envars passed in through --env-file
    [skip ci]

--- published-scripts/docker-job.sh
+++ published-scripts/docker-job.sh
@@ -1,36 +1,24 @@
 #!/bin/sh
 # Run a build job in Docker
 #
-# Requires:
-#  BUILD_ENVIRONMENT_IMAGE
-#  CONTAINER_PATH
-#  REF_TAG
-#  BUILD_ID
-#  BASE_BUILD_IMAGE
-#  IMAGE_SOURCE_REPOSITORY
-#  IMAGE_REVISION
-#  DOCKER_REGISTRY
-#  DOCKER_REGISTRY_PASSWORD
-#  DOCKER_REGISTRY_USERNAME
-#  CONTAINER_SCRIPT
+# Requires environment variables:
+#   BUILD_ENVIRONMENT_IMAGE
+#   CONTAINER_PATH
+#   CONTAINER_SCRIPT
+#   ENV_FILE - Path to a file with the additional environment variables you wish to pass to the container
 #
-# Then execute:
+# For more information on ENV_FILE, see:
+#   https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file
+#
+# Once set up, execute:
 #   curl -sSL https://gitlab.com/cssl/NetChris/public/build/docker/images/raw/master/published-scripts/docker-job.sh | sh -e
 
-# exit immediately when a command fails
-# treat unset variables as an error and exit immediately
+# exit immediately when a command fails and treat unset variables as an error and exit immediately
 set -eu
 
 docker run --rm \
   -v $(pwd):${CONTAINER_PATH} \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  --env REF_TAG \
-  --env BUILD_ID \
-  --env BASE_BUILD_IMAGE \
-  --env IMAGE_SOURCE_REPOSITORY \
-  --env IMAGE_REVISION \
-  --env DOCKER_REGISTRY \
-  --env DOCKER_REGISTRY_PASSWORD \
-  --env DOCKER_REGISTRY_USERNAME \
+  --env-file ${ENV_FILE} \
   ${BUILD_ENVIRONMENT_IMAGE} \
   /bin/sh -c "cd ${CONTAINER_PATH} && ${CONTAINER_SCRIPT}"
