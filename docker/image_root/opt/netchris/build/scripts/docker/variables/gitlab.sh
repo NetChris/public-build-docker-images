07195831a5e59fd6f100813bea13ed58a62484af
Chris Simmons <chris.dev@netchris.com>
2020-01-01 12:57:05 -0800

    TODO
    [skip ci]

--- docker/image_root/opt/netchris/build/scripts/docker/variables/gitlab.sh
+++ docker/image_root/opt/netchris/build/scripts/docker/variables/gitlab.sh
@@ -7,5 +7,12 @@ export BASE_BUILD_IMAGE="${CI_REGISTRY_IMAGE}"
 export IMAGE_SOURCE_REPOSITORY="${CI_PROJECT_URL}"
 export IMAGE_REVISION="${CI_COMMIT_SHA}"
 export DOCKER_REGISTRY="${CI_REGISTRY}"
+
+# TODO - This is problematic:
+# It's fine for image pushes to the local registry, but
+# it is misleading for deployment.  The deployments will
+# often work, but then if those services need to restart themselves
+# these credentials will be long-gone.  We need something to explicitly
+# pull in GitLab variables for image push vs. deployment.
 export DOCKER_REGISTRY_PASSWORD="${CI_REGISTRY_PASSWORD}"
 export DOCKER_REGISTRY_USERNAME="${CI_REGISTRY_USER}"
