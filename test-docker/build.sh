382095771688d95b4d33c96398b957de1a6d8afb
Chris Simmons <chris.dev@netchris.com>
2020-01-02 09:58:41 -0800

    Fix test

--- test-docker/build.sh
+++ test-docker/build.sh
@@ -5,8 +5,9 @@
 
 export VARIANT="test-docker"
 export IMAGE_BUILD_ID_PREFIX="1.2.3"
-export IMAGE_PRODUCT_ID="cssl/netchris/public/build/docker/images/${VARIANT}"
-. "./${VARIANT}/variables.sh"
+export IMAGE_PRODUCT_ID="cssl/netchris/public/build/docker/images/${IMAGE_BUILD_SUBDIRECTORY}"
+export IMAGE_BUILD_SUBDIRECTORY=${IMAGE_BUILD_SUBDIRECTORY}
+. "./${IMAGE_BUILD_SUBDIRECTORY}/variables.sh"
 
 . "/opt/netchris/build/scripts/docker/variables/build.sh"
 . "/opt/netchris/build/scripts/docker/functions.sh"
