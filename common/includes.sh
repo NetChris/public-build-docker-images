0c3a1ef904807ad7895d3cb22001372da5987071
Chris Simmons <chris.dev@netchris.com>
2022-01-03 18:50:06 -0800

    Refactor

--- common/includes.sh
+++ common/includes.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-. "./common/sets.sh"
+# See https://stackoverflow.com/a/2871034/208990
+# exit immediately when a command fails
+set -e
+# treat unset variables as an error and exit immediately
+set -u
+
 . "./common/variables.sh"
 . "./common/functions.sh"
--- common/sets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# See https://stackoverflow.com/a/2871034/208990
-
-# exit immediately when a command fails
-set -e
-
-# treat unset variables as an error and exit immediately
-set -u
