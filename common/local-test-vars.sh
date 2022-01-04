2f6acf28e6e477e594d3635b4f6e1e75b9894802
Chris Simmons <chris.dev@netchris.com>
2022-01-03 18:02:22 -0800

    Use CI_COMMIT_REF_SLUG instead of CI_COMMIT_REF_NAME

--- common/local-test-vars.sh
+++ common/local-test-vars.sh
@@ -3,12 +3,12 @@
 export CI_REPOSITORY_URL="https://gitlab.com/cssl/NetChris/public/build/docker/images.git"
 export CI_PROJECT_URL="https://gitlab.com/cssl/NetChris/public/build/docker/images"
 export CI_REGISTRY_IMAGE="registry.gitlab.com/cssl/netchris/public/build/docker/images"
-export CI_COMMIT_REF_NAME=main
+export CI_COMMIT_REF_SLUG=main
 export CI_PIPELINE_ID="12345"
 export CI_COMMIT_SHA=deadbeefdeadbeefdeadbeefdeadbeefdeadbeef
 
 export BASE_BUILD_IMAGE=${CI_REGISTRY_IMAGE}
-export REF_TAG=${CI_COMMIT_REF_NAME}
+export REF_TAG=${CI_COMMIT_REF_SLUG}
 export BUILD_ID=${CI_PIPELINE_ID}
 export BUILD_URL="https://gitlab.com/cssl/NetChris/public/build/docker/images/-/pipelines/${CI_PIPELINE_ID}"
 export DOCKER_IMAGE_URL=${CI_PROJECT_URL}
